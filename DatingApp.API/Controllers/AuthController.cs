6b31e293bb5319829ebe9a1bc729b3acb542fe87
qwe3804132 <liupeiran9324@outlook.com>
2019-07-20 11:17:21 -0400

    add JWT and Bearer

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,12 @@ using DatingApp.API.Data;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using DatingApp.API.Dtos;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace DatingApp.API.Controllers
 
@@ -14,13 +20,16 @@ namespace DatingApp.API.Controllers
     public class AuthController:ControllerBase
     {
         private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository repo){
+        public AuthController(IAuthRepository repo,IConfiguration config)
+        {
+            _config = config;
             _repo = repo;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto){
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto){
             
             userForRegisterDto.Username=userForRegisterDto.Username.ToLower();
             if (await _repo.UserExists(userForRegisterDto.Username))
@@ -32,9 +41,50 @@ namespace DatingApp.API.Controllers
                 Username = userForRegisterDto.Username
             };
 
-            var createdUser = await _repo.Register(userToCreate,userForRegisterDto.Username);
+            var createdUser = await _repo.Register(userToCreate,userForRegisterDto.Password);
 
             return StatusCode(201);
+
+
+        }
+        
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(),userForLoginDto.Password);
+
+            if(userFromRepo==null)
+            return Unauthorized();
+            
+            var claims = new[]
+            {
+                    new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name,userFromRepo.Username)
+            };
+
+
+            var key= new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials=creds
+            };
+
+            var tokenHandler= new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            
+            return Ok(new{token = tokenHandler.WriteToken(token)});
+
+
+
+
+
 
 
         }
--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
-    {
+    {   [Required]
         public string Username { get; set; }
+        [Required]
+        [StringLength(8,MinimumLength=4,ErrorMessage="Your must have strong PASSWORD that longer than 4")]
         public string Password { get; set; }
     }
 }
