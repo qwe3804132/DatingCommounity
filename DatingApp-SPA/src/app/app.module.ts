8c737b15d3e243d66d0a72f3b66eb224b789c082
qwe3804132 <liupeiran9324@outlook.com>
2019-07-21 16:24:38 -0400

    added Route and Home message component

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { AlertifyService } from './_services/alertify.service';
 import { ErrorInterceptorProvider } from './_services/ErrorInterceptor';
 import { AuthService } from './_services/auth.service';
@@ -13,6 +14,11 @@ import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+
 
 
 @NgModule({
@@ -20,14 +26,19 @@ import { BsDropdownModule } from 'ngx-bootstrap';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
+
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { HomeComponent } from './home/home.component';
+import { Component } from '@angular/core';
+
+
+export const appRoutes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'members', component: MemberListComponent},
+    {path: 'messages', component: MessagesComponent},
+    {path: 'lists', component: ListsComponent},
+    {path: '**', redirectTo: 'home', pathMatch: 'full'},
+
+];
