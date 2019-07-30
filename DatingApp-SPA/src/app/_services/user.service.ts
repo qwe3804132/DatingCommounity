b171160579f676b5af837eaefa7060e77ad78d3e
qwe3804132 <liupeiran9324@outlook.com>
2019-07-30 13:39:55 -0400

    fix bugs for Case import address

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from '../_models/User';
+import { User } from '../_models/user';
 
 const httpOptions = {
   headers: new HttpHeaders({
