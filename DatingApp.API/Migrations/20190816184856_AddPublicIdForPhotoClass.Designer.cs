08ce05c1196bc6757590e5636e930a8b27394350
qwe3804132 <liupeiran9324@outlook.com>
2019-08-16 14:53:57 -0400

    installed CloudinaryDotNet Package, Configure settings

--- DatingApp.API/Migrations/20190816184856_AddPublicIdForPhotoClass.Designer.cs
+++ DatingApp.API/Migrations/20190816184856_AddPublicIdForPhotoClass.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DatingApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingAPP.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190816184856_AddPublicIdForPhotoClass")]
+    partial class AddPublicIdForPhotoClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -43,6 +43,7 @@ namespace DatingAPP.API
                     });
 
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper();
             services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository,AuthRepository>();
